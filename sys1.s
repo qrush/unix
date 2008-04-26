7192d54912f625b56ad738f3314a8d0aa3abdcda
tim.newsham <tim.newsham@44b2186c-a14b-0410-8c95-595313601b93>
2008-04-26 19:15:52 +0000

    - syscall table for 1st ed.
    git-svn-id: http://unix-jun72.googlecode.com/svn/trunk@63 44b2186c-a14b-0410-8c95-595313601b93

---
+++ sys1.s
@@ -0,0 +1,37 @@
+rele    = 0.
+exit    = 1.
+fork    = 2.
+read    = 3.
+write   = 4.
+open    = 5.
+close   = 6.
+wait    = 7.
+creat   = 8.
+link    = 9.
+unlink  = 10.
+exec    = 11.
+chdir   = 12.
+time    = 13.
+mkdir   = 14.
+chmod   = 15.
+chown   = 16.
+break   = 17.
+stat    = 18.
+seek    = 19.
+tell    = 20.
+/ mount is defined as a label in ux.s.
+/ mount = 21.
+umount  = 22.
+setuid  = 23.
+getuid  = 24.
+stime   = 25.
+quit    = 26.
+intr    = 27.
+fstat   = 28.
+emt     = 29.
+mdate   = 30.
+smdate  = 30.
+stty    = 31.
+gtty    = 32.
+ilgins  = 33.
+
