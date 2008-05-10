0dc6b37c8059b9cd8614697f802861e10943e6eb
tim.newsham <tim.newsham@44b2186c-a14b-0410-8c95-595313601b93>
2008-05-10 18:33:06 +0000

    - oops, wrong text size in fixaout.py.  Needs to accomodate the   extra 12 bytes of the a.out header that werent part of the text   size before.
    git-svn-id: http://unix-jun72.googlecode.com/svn/trunk@212 44b2186c-a14b-0410-8c95-595313601b93

--- tools/fixaout.py
+++ tools/fixaout.py
@@ -27,5 +27,5 @@ def write(fn, d) :
 
 d1 = words(read('a.out'))
 hdr = d1[:8]
-d = [0405, hdr[1], 0, 0, hdr[4], 0] + d1[8:]
+d = [0405, 12+hdr[1], 0, 0, hdr[4], 0] + d1[8:]
 write("b.out", unwords(d))
